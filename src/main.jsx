76ee4d3e31a31953e7a5f0d553e3a06cd04690a4
Hetong Wang <u7599107@anu.edu.au>
2025-07-29 13:18:15 +1000

    Initial commit

---
+++ src/main.jsx
@@ -0,0 +1,39 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import './index.css'
+import ErrorPage from "./error-page";
+import Contact,{
+  loader as contactLoader, 
+} from "./routes/contact";
+import Root, { loader as rootLoader, action as rootAction, } from "./routes/root";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root/>,
+    errorElement: <ErrorPage />,
+    loader: rootLoader,
+    action: rootAction,
+    children: [
+      {
+        path: "contacts/:contactId",
+        element: <Contact />,
+        loader: contactLoader,
+      },
+    ],
+  },
+  {
+    path: "contacts/:contactId",
+    element: <Contact />,
+  },
+]);
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
+)
